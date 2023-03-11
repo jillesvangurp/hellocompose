00f7efc6887a1f33436cc4c22a7d52fb4f687b4d
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2023-03-11 06:00:26 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "com.jillesvangurp"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+
+                implementation(npm("tailwindcss", "3.2.7")) // Official tailwindcss package
+                implementation(npm("@tailwindcss/forms", "0.5.3"))
+                implementation(npm("postcss", "8.4.21")) // CSS processor called postcss
+                implementation(npm("autoprefixer", "10.4.14")) //parse CSS and add browser specific prefixes to CSS rules
+
+                implementation(npm("style-loader", "3.3.1"))
+                implementation(npm("css-loader", "6.7.3")) // required to handle @import/url() rules in CSS files
+                implementation(npm("postcss-loader", "7.0.2")) // required to invoke postcss during bundling
+                implementation(devNpm("cssnano", "5.1.15"))            }
+
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+
+rootProject.name = "hellocompose"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+plugins {
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
